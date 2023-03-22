3bf73b3a014aa67c62cadd81254d7c44f63e4064
Kirill Mitkovskii <kirill.mitkovskii@gmail.com>
2023-03-22 17:53:37 -0400

    Feat WIP adding carla_ros_bridge remapping. Copy carla_ros_bridge and add channels remapping

--- launch/carla_ros_bridge.launch.py
+++ launch/carla_ros_bridge.launch.py
@@ -1,36 +1,113 @@
-import os
-
 import launch
-from launch.actions import GroupAction
-from launch.actions import SetRemap
-from launch.actions import IncludeLaunchDescription
-from launch.actions import DeclareLaunchArgument
-from ament_index_python.packages import get_package_share_directory
+import launch_ros.actions
 
 
 def generate_launch_description():
-    
-    # carla_ros_bridge_loader = launch.LaunchDescription([
-    #     IncludeLaunchDescription(
-    #         launch.launch_description_sources.PythonLaunchDescriptionSource(
-    #             os.path.join(get_package_share_directory(
-    #                 'carla_ros_bridge'), 'carla_ros_bridge.launch.py')
-    #         )
-    #     )
-    # ])
-    
-    # ToDo(Robotics010): Change to launch with SetRemap when switch to Foxy or later
-    # group = GroupAction(actions=[
-    #     SetRemap(src='/carla/ego_vehicle/rgb_view/camera_info',dst='/sensing/camera/traffic_light/camera_info'),
-    #     SetRemap(src='/carla/ego_vehicle/rgb_view/image',dst='/sensing/camera/traffic_light/image_raw'),
-    #     SetRemap(src='/carla/ego_vehicle/gnss',dst='/sensing/gnss/ublox/nav_sat_fix'),
-    #     SetRemap(src='/carla/ego_vehicle/imu',dst='/sensing/imu/tamagawa/imu_raw'),
-    #     SetRemap(src='/carla/ego_vehicle/lidar',dst='/sensing/lidar/top/pointcloud_raw_ex'),
-    #     SetRemap(src='/carla/ego_vehicle/velocity_status',dst='/vehicle/status/velocity_status'),
-    #     carla_ros_bridge_loader,
-    # ])
-    
-    return group
+    ld = launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(
+            name='host',
+            default_value='localhost',
+            description='IP of the CARLA server'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='port',
+            default_value='2000',
+            description='TCP port of the CARLA server'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='timeout',
+            default_value='2',
+            description='Time to wait for a successful connection to the CARLA server'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='passive',
+            default_value='False',
+            description='When enabled, the ROS bridge will take a backseat and another client must tick the world (only in synchronous mode)'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='synchronous_mode',
+            default_value='True',
+            description='Enable/disable synchronous mode. If enabled, the ROS bridge waits until the expected data is received for all sensors'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='synchronous_mode_wait_for_vehicle_control_command',
+            default_value='False',
+            description='When enabled, pauses the tick until a vehicle control is completed (only in synchronous mode)'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='fixed_delta_seconds',
+            default_value='0.05',
+            description='Simulation time (delta seconds) between simulation steps'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='town',
+            default_value='Town01',
+            description='Either use an available CARLA town (eg. "Town01") or an OpenDRIVE file (ending in .xodr)'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='register_all_sensors',
+            default_value='True',
+            description='Enable/disable the registration of all sensors. If disabled, only sensors spawned by the bridge are registered'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name='ego_vehicle_role_name',
+            default_value=["hero", "ego_vehicle", "hero0", "hero1", "hero2",
+                           "hero3", "hero4", "hero5", "hero6", "hero7", "hero8", "hero9"],
+            description='Role names to identify ego vehicles. '
+        ),
+        launch_ros.actions.Node(
+            package='carla_ros_bridge',
+            executable='bridge',
+            name='carla_ros_bridge',
+            output='screen',
+            emulate_tty='True',
+            on_exit=launch.actions.Shutdown(),
+            parameters=[
+                {
+                    'use_sim_time': True
+                },
+                {
+                    'host': launch.substitutions.LaunchConfiguration('host')
+                },
+                {
+                    'port': launch.substitutions.LaunchConfiguration('port')
+                },
+                {
+                    'timeout': launch.substitutions.LaunchConfiguration('timeout')
+                },
+                {
+                    'passive': launch.substitutions.LaunchConfiguration('passive')
+                },
+                {
+                    'synchronous_mode': launch.substitutions.LaunchConfiguration('synchronous_mode')
+                },
+                {
+                    'synchronous_mode_wait_for_vehicle_control_command': launch.substitutions.LaunchConfiguration('synchronous_mode_wait_for_vehicle_control_command')
+                },
+                {
+                    'fixed_delta_seconds': launch.substitutions.LaunchConfiguration('fixed_delta_seconds')
+                },
+                {
+                    'town': launch.substitutions.LaunchConfiguration('town')
+                },
+                {
+                    'register_all_sensors': launch.substitutions.LaunchConfiguration('register_all_sensors')
+                },
+                {
+                    'ego_vehicle_role_name': launch.substitutions.LaunchConfiguration('ego_vehicle_role_name')
+                }
+            ],
+            remappings=[
+                ('/carla/ego_vehicle/rgb_front/camera_info', '/sensing/camera/traffic_light/camera_info'),
+                ('/carla/ego_vehicle/rgb_front/image', '/sensing/camera/traffic_light/image_raw'),
+                ('/carla/ego_vehicle/gnss', '/sensing/gnss/ublox/nav_sat_fix'),
+                ('/carla/ego_vehicle/imu', '/sensing/imu/tamagawa/imu_raw'),
+                ('/carla/ego_vehicle/lidar', '/sensing/lidar/top/pointcloud_raw_ex'),
+                ('/carla/ego_vehicle/velocity_status', '/vehicle/status/velocity_status'),
+            ],
+        )
+    ])
+    return ld
 
 
 if __name__ == '__main__':
